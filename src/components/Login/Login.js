c6b1fd898c64dfdde476a91dc8cbe1271b025db9
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-19 11:59:54 -0800

    added pages, changed index html

---
+++ src/components/Login/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="form-group">
+        <label for="username">
+          <h4> Username </h4>
+        </label>
+        <input
+          type="username"
+          className="form-control"
+          style=" margin-top: 5px"
+          id="username-input"
+          placeholder="Username"
+        />
+      </div>
+
+      <div className="form-group" style="margin-top: 10px">
+        <label for="password">
+          <h4>Password</h4>
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password-input"
+          placeholder="Password"
+        />
+      </div>
+
+      <div
+        id="alert"
+        className="alert alert-danger"
+        role="alert"
+      >
+        <span
+          className="glyphicon glyphicon-exclamation-sign"
+          aria-hidden="true"
+        ></span>
+        <span className="sr-only">Error:</span> <span className="msg"></span>
+      </div>
+
+      <button
+        type="submit"
+        className="ui inverted teal basic button"
+      >
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;

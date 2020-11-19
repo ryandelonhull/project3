347b4560fc7bf732345f2f3766d903584ce34f6d
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-19 14:05:21 -0800

    schema done, made some changes to package.json might want to ignore who knows?

--- migration/schema.sql
+++ migration/schema.sql
@@ -7,41 +7,172 @@ CREATE TABLE Characters
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL unique,
 	image varchar(255) NOT NULL unique,
-    attacks  varchar(255) NOT NULL,
+    attacks  integer NOT NULL,
     
 	PRIMARY KEY (id)
 );
 
 Insert into Characters (name, image, attacks)
-VALUES("Batman","https://media2.giphy.com/media/26vUCH220kcToAYPS/giphy.gif", "Crying");
+VALUES("Batman","https://media2.giphy.com/media/26vUCH220kcToAYPS/giphy.gif", "1");
 Insert into Characters (name, image, attacks)
-VALUES("House Plant","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgITFpVbSNOVWCrVgtoBD1CidxqH0hm4eOeA&usqp=CAU", "Roots");
+VALUES("House Plant","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgITFpVbSNOVWCrVgtoBD1CidxqH0hm4eOeA&usqp=CAU", "2");
 Insert into Characters (name, image, attacks)
-VALUES("Barack Obama","https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950", "DunksOnYou");
+VALUES("Barack Obama","https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950", "3");
 Insert into Characters (name, image, attacks)
-VALUES("Ninja","https://pbs.twimg.com/profile_images/1168269614058311680/qVZrY0PL_400x400.jpg", "Sneaky Sneaky");
+VALUES("Ninja","https://pbs.twimg.com/profile_images/1168269614058311680/qVZrY0PL_400x400.jpg", "4");
 Insert into Characters (name, image, attacks)
-VALUES("LA DRIVER","none", "Quick Brake");
+VALUES("LA DRIVER","none", "5");
 Insert into Characters (name, image, attacks)
-VALUES("Stationary Exericse Bike","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmj9TIKZiDTKp8hx22bUHWV6W9ESkFFWk6qA&usqp=CAU", "Butt Cramps");
+VALUES("Stationary Exericse Bike","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmj9TIKZiDTKp8hx22bUHWV6W9ESkFFWk6qA&usqp=CAU", "6");
 Insert into Characters (name, image, attacks)
-VALUES("Seth Rogan (and friend)","https://hips.hearstapps.com/esquireuk.cdnds.net/15/37/original/original-seth-rogan-james-franco-kanye-west-parody-43-jpg-f09da4b6.jpg", "Blaze Up");
+VALUES("Hot Dog","https://www.ballparkbrand.com/sites/default/files/Hero_Dog_0.png", "7");
 Insert into Characters (name, image, attacks)
-VALUES("Hot Dog","https://www.ballparkbrand.com/sites/default/files/Hero_Dog_0.png", "KetchupOnYourShirt");
+VALUES("Seth Rogan (and friend)","https://hips.hearstapps.com/esquireuk.cdnds.net/15/37/original/original-seth-rogan-james-franco-kanye-west-parody-43-jpg-f09da4b6.jpg", "8");
 Insert into Characters (name, image, attacks)
-VALUES("Rick Astley","https://imgs.smoothradio.com/images/191589?crop=16_9&width=660&relax=1&signature=Rz93ikqcAz7BcX6SKiEC94zJnqo=", "Never Give You Up");
-
-
+VALUES("Rick Astley","https://imgs.smoothradio.com/images/191589?crop=16_9&width=660&relax=1&signature=Rz93ikqcAz7BcX6SKiEC94zJnqo=", "9");
+Insert into Characters (name, image, attacks)
+VALUES("Wess","https://avatars0.githubusercontent.com/u/70366408?s=460&u=3e7affbb1dd65a64e2aaebfaa65272951dd598d5&v=4", "10");
+Insert into Characters (name, image, attacks)
+VALUES("HypnoToad","https://avatars0.githubusercontent.com/u/70366408?s=460&u=3e7affbb1dd65a64e2aaebfaa65272951dd598d5&v=4", "11");
+Insert into Characters (name, image, attacks)
+VALUES("PE Teacher","https://avatars0.githubusercontent.com/u/70366408?s=460&u=3e7affbb1dd65a64e2aaebfaa65272951dd598d5&v=4", "12");
 Select * from Characters;
 
 CREATE TABLE Attacks
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL unique,
-	CharacterId varchar(255) NOT NULL,
+	CharacterId integer NOT NULL,
 
-    
+    foreign key (CharacterId) references Characters (id),
 	PRIMARY KEY (id)
 );
 
+Insert into Attacks (name, CharacterId)
+VALUES("Crying", "1");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Bat Mobile", "1");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Bruding and Smouldering", "1");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Daddy Issues", "1");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Ask Alfred", "1");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Drop Leaves", "2");
+Insert into Attacks (name, CharacterId)
+VALUES("Leak Water", "2");
+Insert into Attacks (name, CharacterId)
+VALUES("Make Clones", "2");
+Insert into Attacks (name, CharacterId)
+VALUES("shrivel", "2");
+Insert into Attacks (name, CharacterId)
+VALUES("Growth Spurt", "2");
+
+Insert into Attacks (name, CharacterId)
+VALUES("DunksONYou", "3");
+Insert into Attacks (name, CharacterId)
+VALUES("DadJokes", "3");
+Insert into Attacks (name, CharacterId)
+VALUES("Tan Suite", "3");
+Insert into Attacks (name, CharacterId)
+VALUES("Swagger", "3");
+Insert into Attacks (name, CharacterId)
+VALUES("Ask Michelle", "3");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Sneaky Sneaky", "4");
+Insert into Attacks (name, CharacterId)
+VALUES("Surprise! Behind You", "4");
+Insert into Attacks (name, CharacterId)
+VALUES("Nunchucks", "4");
+Insert into Attacks (name, CharacterId)
+VALUES("Karate Chop", "4");
+Insert into Attacks (name, CharacterId)
+VALUES("Roundhouse Kick", "4");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Break Check", "5");
+Insert into Attacks (name, CharacterId)
+VALUES("Cut Off", "5");
+Insert into Attacks (name, CharacterId)
+VALUES("Surpise Turn", "5");
+Insert into Attacks (name, CharacterId)
+VALUES("Change 3 lanes at once", "5");
+Insert into Attacks (name, CharacterId)
+VALUES("Changes Lanes into You", "5");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Butt Cramps", "6");
+Insert into Attacks (name, CharacterId)
+VALUES("Toe Stub", "6");
+Insert into Attacks (name, CharacterId)
+VALUES("Dehydration", "6");
+Insert into Attacks (name, CharacterId)
+VALUES("wack your dog with the pedals", "6");
+Insert into Attacks (name, CharacterId)
+VALUES("Fall Off", "6");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Ketchup on Your Shirt", "7");
+Insert into Attacks (name, CharacterId)
+VALUES("Too Good to Resist", "7");
+Insert into Attacks (name, CharacterId)
+VALUES("Not Enough Bun son", "7");
+Insert into Attacks (name, CharacterId)
+VALUES("Can't have just one", "7");
+Insert into Attacks (name, CharacterId)
+VALUES("Food Poisoning", "7");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Blaze up", "8");
+Insert into Attacks (name, CharacterId)
+VALUES("Sorry", "8");
+Insert into Attacks (name, CharacterId)
+VALUES("Weird Laughing", "8");
+Insert into Attacks (name, CharacterId)
+VALUES("Red Eye Snack Attack", "8");
+Insert into Attacks (name, CharacterId)
+VALUES("Bagged Milk!!", "8");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Never gonna give you up", "9");
+Insert into Attacks (name, CharacterId)
+VALUES("Never gonna let you down", "9");
+Insert into Attacks (name, CharacterId)
+VALUES("Never gonna run around and desert you", "9");
+Insert into Attacks (name, CharacterId)
+VALUES("Never gonna make you cry", "9");
+Insert into Attacks (name, CharacterId)
+VALUES("Never gonna say goodbye", "9");
+
+Insert into Attacks (name, CharacterId)
+VALUES("Hypnotize", "11");
+Insert into Attacks (name, CharacterId)
+VALUES("Stupify", "11");
+Insert into Attacks (name, CharacterId)
+VALUES("Mesmorize", "11");
+Insert into Attacks (name, CharacterId)
+VALUES("Confound", "11");
+Insert into Attacks (name, CharacterId)
+VALUES("Bewilder", "11");
+
+
+Insert into Attacks (name, CharacterId)
+VALUES("DODGEBALL!", "12");
+Insert into Attacks (name, CharacterId)
+VALUES("RUN LAPS", "12");
+Insert into Attacks (name, CharacterId)
+VALUES("CLIMB THE ROPE", "12");
+Insert into Attacks (name, CharacterId)
+VALUES("SEX ED", "12");
+Insert into Attacks (name, CharacterId)
+VALUES("Being Creepy", "12");
+
 Select * from Attacks
+

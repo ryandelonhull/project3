f1c7ce8e42924f9f77d6cf1e8a8d9b606c162884
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-23 13:59:34 -0800

    ignore me

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+function Contact() {
+    return (
+    <div className="container col-sm-10 col-md-8 col-lg-8 mr-20 ml-2> ">
+        <div className="card-row">
+          <div className="card-body">
+            <h4 className="card-title">Contact</h4>
+           
+            <p className="card-text"/>
+              <div className="image-wrapper float-left pr-3">
+                <div className="card-body">
+
+             </div>
+             <div className="card-text"  className="single-post-content-wrapper p-3">
+             <p> I am currently a student at UC Berkeley's Extension Full Stack Web Developer Certificate Program. I have BS in
+              Environmental Science and Chemistry from The Evergreen State College that I received in 2011. I have passion for solving problems using logic and critical thinking. I have never been afraid big projects or big problems and have yet to meet one that I can't find a creative solution for. I like to find the beauty in an algorithm or a formula. I am ready to apply my methodical and reasoned approach to anything put in front of me. 
+            
+            </p>      
+            <p>
+              Skills: HTML, CSS, JavaScript, jQuery, node.js, python, express.js, mysql, sequelize, mongdb, mongoose, chart.js, p5.js Restful Routes, handlebars, API's, axios, Heavy Metal Analysis using ICP-OES, ICP-MS
+              </p>
+           
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    )
+
+}
+
+export default Contact

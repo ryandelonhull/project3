c15b230390238407c02d50d06ce3d09fc511e8e4
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-23 11:56:57 -0800

    fixed server errors

---
+++ client/src/pages/LoginSignUP.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Login from '../components/Login/Login'
+
+class LoginSignUp extends Component{
+    render(){
+        return(
+            <div>
+                {/* Login sign up components go here */}
+                <Login/>
+            </div>
+        )
+    }
+}
+
+export default LoginSignUp;
--- routes/api/profile-api-routes.js
+++ routes/api/profile-api-routes.js
@@ -1,5 +1,5 @@
 // Requiring our models
-var db = require("../models");
+var db = require("../../models");
 
 // Routes
 module.exports = function(app) {

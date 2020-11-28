b33857619681129011d2688e45d0a40ce760cf2f
Slice Wess <ernestwesson86@gmail.com>
2020-11-27 20:41:29 -0800

    changed package json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Battle from "./components/Battle/Battle";
+import NavBar from "./components/BattleStyles/NavBar"
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <h2>Project X - The Ultimate Battle!</h2>
+          <NavBar/>
           {/* <Route path="/" exact component={Index} /> */}
           {/* <Route path="/Profile" component={Profile} />
           <Route path="/Signup" component={Signup} />

540201ed0328a53a9d833a8506cc159fb5cb8129
Slice Wess <ernestwesson86@gmail.com>
2020-11-28 21:09:05 -0800

    finally fixed the dang nav-links to the left

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './Navbar.css';
 
 
+
+
 function Navbar() {
   return (
+   
+   
+   
+   
    <div>
-      
-      
-      <nav className="navbar navbar-expand-sm">
-      <div className="App">
-        {/* <img src={require("./projectxlogo.png")} alt="Navbar Image" /> */}
-        <img src="https://i.ibb.co/VMjK21c/projectxlogo150x51.png" />
-        {/* <img src="./projectxlogo.png" thumbnail /> */}
-        
-      <ul className="nav justify-content-end">
+     
+      <nav className="navbar">
+      <img src="https://i.ibb.co/VMjK21c/projectxlogo150x51.png"/>
+      <ul className="nav">
         <li className="nav-item">
-          {/* <a className="nav-link active" href="#">
+          <a className="nav-link active" href="#">
             About
-          </a> */}
+          </a>
         </li>
         <li className="nav-item">
           {/* /I just added profile here for now can change it later */}
@@ -32,6 +33,13 @@ function Navbar() {
          
         </li>
       </ul>
+      <div className="App">
+        {/* <img src={require("./projectxlogo.png")} alt="Navbar Image" /> */}
+        {/* <img src="https://i.ibb.co/VMjK21c/projectxlogo150x51.png" /> */}
+        {/* <img src="./projectxlogo.png" thumbnail /> */}
+        
+        
+    
       
       </div>
       </nav>

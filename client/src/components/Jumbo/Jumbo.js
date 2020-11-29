a246280e59abe315b40be80e82502b9be1c3a837
Slice Wess <ernestwesson86@gmail.com>
2020-11-28 21:16:27 -0800

    finishing adjustments with button placement

--- client/src/components/Jumbo/Jumbo.js
+++ client/src/components/Jumbo/Jumbo.js
@@ -7,7 +7,7 @@ const Jumbo = () => {
         <div>
            <div className="jumbotron">
            {/* <img src="https://i.ibb.co/PhV3Jwx/project-Xjumbo.png" /> */}
-            <a className="btn btn-danger btn-xlg" href="#" role="button">SIGN IN</a>
+            <a className="btn btn-danger btn-lg" href="#" role="button">SIGN IN</a>
             
             </div> 
         </div>

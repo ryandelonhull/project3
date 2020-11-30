5a2159c1348c02cc17b8e4560596d4d2b5170281
Slice Wess <ernestwesson86@gmail.com>
2020-11-29 20:01:37 -0800

    completed about section

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './About.css'
 
 function About() {
     return(
-        <div>
+        <div className="About">
+            
             <p> Here is where we are going to place the about</p>
+            
         </div>
     )
 }

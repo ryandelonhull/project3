b806b836617c07c9e7b477469ec73261d2673c67
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-30 10:21:42 -0800

    yet another deploy attempt

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
       <img src="https://i.ibb.co/VMjK21c/projectxlogo150x51.png" alt="project x"/>
       <ul className="nav">
         <li className="nav-item">
-          <a className="nav-link active" href="#">
+          <a className="nav-link active" href="#Profile">
             About
           </a>
 

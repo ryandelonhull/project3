e4ede8ca2aa302092208f5a5a32b4ee9be27cc98
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-30 16:16:31 -0800

    float right navbar items :)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -9,61 +9,39 @@ function Navbar(props) {
     history.push('/');
   };
   const handleContactClick = () => {
-    history.push('/Contact')
-  }
+    history.push('/Contact');
+  };
 
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg">
-        <img src="https://i.ibb.co/VMjK21c/projectxlogo150x51.png" alt="logo" />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link active mr-sm-2" href='/' onClick={handleHomeClick}>
+      <nav className="navbar fixed-top navbar-expand">
+        <img src="./Images/projectxlogo.png" alt="logo" />
+        <ul className="nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link active" href="/" onClick={handleHomeClick}>
               Home
             </a>
-            <a className="nav-link" href='/About'>About</a>
-            <a className="nav-link" href='/Contact' onClick={handleContactClick}>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/About">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="/Contact"
+              onClick={handleContactClick}
+            >
               Contact
             </a>
-          </div>
-        </div>
-        {/* <ul className="nav">
-        <li className="nav-item">
-          <a className="nav-link active" href="#Profile">
-            About
-          </a>
-
-        </li>
-        <li className="nav-item"> */}
-        {/* /I just added profile here for now can change it later */}
-        {/* <a className="nav-link" href="#Profile">
-            Profile
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#Contact">
-            Contact
-          </a>
-
-         
-        </li>
-      </ul> */}
-        <div className="App">
-          {/* <img src={require("./projectxlogo.png")} alt="Navbar Image" /> */}
-          {/* <img src="https://i.ibb.co/VMjK21c/projectxlogo150x51.png" /> */}
-          {/* <img src="./projectxlogo.png" thumbnail /> */}
-        </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#signup">
+              Sign Up/Sign In
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );

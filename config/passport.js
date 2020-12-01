14e244503ba6085702dabfd41e9cc190d0aecc8f
Jennifer Henry <jenhenry1995@gmail.com>
2020-12-01 15:34:19 -0800

    quick try

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ var db = require("../models");
 passport.use(new LocalStrategy(
   // Our user will sign in using an email, rather than a "username"
   {
-    usernameField: "email"
+    usernameField: "username"
   },
   function(username, password, done) {
     // When a user tries to sign in this code runs

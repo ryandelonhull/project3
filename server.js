afd55d996f46bb456309ac181f353bf1f1ea3b25
Ryan Hull <ryandelonhull@gmail.com>
2020-12-02 18:29:53 -0500

    deploy to heroku

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  } else {
+    app.use(express.static('client/public'));
+  }
+
 // We need to use sessions to keep track of our user's login status
 
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
